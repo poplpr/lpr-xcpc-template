d0c5c165a3c2bb7820422565e641e68900be2504
poplpr <289908467@qq.com>
2024-12-08 16:57:43 +0800

    base template 更新

--- base-template.cpp
+++ base-template.cpp
@@ -6,9 +6,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-using db = double;
-using ld = long double;
+using i64 = long long;
+using i128 = __int128;
+using f64 = double;
+using f128 = long double;
 using pii = pair<int, int>;
 
 #define fi first
@@ -17,8 +18,33 @@ using pii = pair<int, int>;
 #define mp make_pair
 #define ALL(x) (x).begin(), (x).end()
 #define SZ(x) (int)(x).size()
-#define dbg(x) cout << #x << " = " << x << ", "
+#define dbg1(x) cout << #x << " = " << x << ", "
 #define dbg2(x) cout << #x << " = " << x << endl
+
+template <typename T>
+void _debug(const char* format, T t) {
+    cerr << format << '=' << t << endl;
+}
+
+template <class First, class... Rest>
+void _debug(const char* format, First first, Rest... rest) {
+    while (*format != ',') cerr << *format++;
+    cerr << '=' << first << ',';
+    _debug(format + 1, rest...);
+}
+
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& V) {
+    os << "[ ";
+    for (const auto& vv : V) os << vv << ", ";
+    os << ']';
+    return os;
+}
+#ifdef LOCAL
+    #define dbg(...) _debug(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define dbg(...) 
+#endif
 
 template<typename T> void read(T &x) {
     x = 0; int f = 1; char c = getchar();
@@ -31,7 +57,7 @@ template<typename T, typename... Args> void read(T &x, Args&... args) {
     read(x); read(args...);
 }
 
-template<typename T> void write(T x) {
+template<typename T> void _write(T x) {
     static int buf[42];
     int p = 0;
     if (x < 0) putchar('-'), x = -x;
@@ -41,6 +67,19 @@ template<typename T> void write(T x) {
         x /= 10;
     }
     for (int i = p - 1; i >= 0; i--) putchar(buf[i] + '0');
+}
+
+template<typename First, typename... Rest> void _write(const First& first, const Rest&... rest) {
+    write(first); putchar(32); write(rest...);
+}
+
+template<typename T> void write(T x, char end_char='\0') {
+    _write(x);
+    if (end_char != '\0') putchar(end_char);
+}
+
+template<typename First, typename... Rest> void write(const First& first, const Rest&... rest) {
+    _write(first); putchar(32); _write(rest...); putchar(10);
 }
 
 void solve() {
@@ -54,7 +93,7 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     int T = 1;
-    // read(T);
+    read(T);
     // cin >> T;
     while (T--) solve();
     return 0;
